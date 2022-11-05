d7970f4ff1bc087d47a155449f939031ec3c5963
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-11-05 14:14:07 +0300

    median function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let nums = vec![1, 3, 5, 7, 11, 26, 13, 4];
+    let nums = vec![1, 3, 5, 7, 11, 26, 4, 2];
 
     let a = avg(&nums);
 
-    println!("{a}")
+    let med = median(&nums);
+
+    println!("{a} {med}")
 }
 
 fn avg(v: &Vec<i32>) -> f64 {
@@ -14,6 +16,11 @@ fn avg(v: &Vec<i32>) -> f64 {
     sum as f64/v.len() as f64
 }
 
-// fn median() {}
+fn median(v: &Vec<i32>) -> i32 {
+    let med_idx = v.len()/2;
+    let mut sorted_vec = v.to_vec();
+    sorted_vec.sort();
+    sorted_vec[med_idx]
+}
 
 // fn mode() {}
