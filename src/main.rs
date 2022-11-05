eae66eccf615538a26cc3bcd76823555cd65a97e
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-11-05 13:58:37 +0300

    average function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![1, 3, 5, 7, 11, 26, 13, 4];
+
+    let a = avg(&nums);
+
+    println!("{a}")
 }
+
+fn avg(v: &Vec<i32>) -> f64 {
+    let mut sum = 0;
+    for num in v {
+        sum += num;
+    }
+    sum as f64/v.len() as f64
+}
+
+// fn median() {}
+
+// fn mode() {}
